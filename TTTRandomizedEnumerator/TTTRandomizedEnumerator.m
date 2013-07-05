de7b40dcb55adfa1d9049205ba7a5a450dbe7c36
Mattt Thompson <m@mattt.me>
2013-07-05 13:26:22 -0500

    Minor formatting

--- TTTRandomizedEnumerator/TTTRandomizedEnumerator.m
+++ TTTRandomizedEnumerator/TTTRandomizedEnumerator.m
@@ -42,7 +42,7 @@
 
     NSMutableArray *mutableObjects = [NSMutableArray arrayWithArray:objects];
     NSUInteger count = [mutableObjects count];
-    // make the shuffling "fair", see http://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+    // See http://en.wikipedia.org/wiki/Fisher–Yates_shuffle
     if (count > 1) {
       for (NSUInteger i = count - 1; i > 0; --i) {
           [mutableObjects exchangeObjectAtIndex:i withObjectAtIndex:arc4random_uniform((int32_t)i)];
